35e5dfed7590fd277b4ab61ba4637df1053f1b9b
Yoonnyy <lotusnewmaker012@gmail.com>
2023-01-07 20:53:50 +0300

    config file

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,19 @@
-import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-
 import router from "./routes.js";
+import config from "../config.js"
 
-dotenv.config();
 
 const app = express();
 
-app.set("PORT", process.env.PORT || 1315);
+app.set("PORT", config.PORT || 1315);
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public", { extensions: ["html"] }));
 
 app.use(router);
 
